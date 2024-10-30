c9fc765d8bc6f45c63dae7447542271b4f7ee81c
David Garcia <david.garcia@wachter.com>
2024-10-30 11:39:41 -0600

    added app insigths

--- obj/Debug/net8.0/net mvc default.AssemblyInfo.cs
+++ obj/Debug/net8.0/net mvc default.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("net mvc default")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c5bfa66276928e368ce306b2679f085a7f195f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("net mvc default")]
 [assembly: System.Reflection.AssemblyTitleAttribute("net mvc default")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
